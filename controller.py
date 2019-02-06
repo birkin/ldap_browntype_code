13d14a992e8d8eb76a9204dde26e8ae46ba0227d
birkin <birkin.diana@gmail.com>
2019-02-06 17:00:40 -0500

    tweaks setup.

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
 """
-Iterates through a file of usernames, and for each:
+Iterates through a file of usernames, and, for each:
 - get the user's LDAP status
 - update ILLiad with the user's status
 """
@@ -7,19 +7,23 @@ Iterates through a file of usernames, and for each:
 import logging, os
 
 
-## settings
 LOG_PATH = os.environ['LDP_BRNTP__LOG_PATH']
 LOG_LEVEL = os.environ['LDP_BRNTP__LOG_LEVEL']
 USER_FILEPATH = os.environ['LDP_BRNTP__USER_FILEPATH']
 TRACKER_FILEPATH = os.environ['LDP_BRNTP__TRACKER_FILEPATH']
 
-## set up file logger
+SKIP_TRACKER_BUILD = False
+
+
+
+
 level_dct = { 'DEBUG': logging.DEBUG, 'INFO': logging.INFO }
 logging.basicConfig(
     filename=LOG_PATH, level=level_dct[LOG_LEVEL],
     format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s', datefmt='%d/%b/%Y %H:%M:%S' )
-logger = logging.getLogger(__name__)
-logger.info( '\n---\n' + 'starting\n' + '---\n' )
+log = logging.getLogger(__name__)
+log.info( '\n---\n' + 'starting\n' + '---\n' )
+
 
 
 1/0
